a3f5d26416e242f63f2a68cf05d2455f400866ec
Brandon Rowe <browe.media@gmail.com>
2019-07-22 23:23:01 -0500

    small tweek

--- server.js
+++ server.js
@@ -161,8 +161,8 @@ botclient.on('chat', (channel, userstate, message, self) => {
 		
 	}
 
-	if (message[0] === '!whosthechillest') {
-		botclient.say(twitchchan[0])
+	if (message[0] === '!whoschill') {
+		botclient.say(twitchchan[0], `Billy is the chill`)
 	}
 })
 
