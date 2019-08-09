7bb405d38ce59132f6d09abc688bb809e348e4ae
Brandon Rowe <browe.media@gmail.com>
2019-08-09 03:36:36 -0500

    Doin the things
    Integrated Pusher for real-time song updates.
    
    Styled Home page (includes video files)
    
    Added Gsap for animations

--- public/js/script.js
+++ public/js/script.js
@@ -2,13 +2,6 @@ const User = document.getElementById('twitchID').innerText
 $('#twitchID').remove();
 console.log(User)
 
-// $(window).ready(function() {
-//   // Animate loader off screen
-//   $(".se-pre-con").fadeOut("slow");;
-// });
-
-// var socket = io.connect(`http://${location.host}/socket.io`);
-
 
 var pusher = new Pusher('94254303a6a5048bf191', {
   cluster: 'us2',
@@ -20,15 +13,16 @@ const srContainer = document.getElementById('srContainer')
 var channel = pusher.subscribe('sr-channel');
 channel.bind('sr-event', function(data) {
   try { 
+    console.log('New Request')
     var srElement = document.createElement('tr')
     // chatMessageId = makeid(5)
     srElement.setAttribute('class', 'song-request')
     srElement.innerHTML = `
-      <td><a href="${data.link}">${data.track} - ${data.artist}</a> <a id="spotify" href="${data.uri}"><i class="fab fa-spotify"></i></a></td>
+      <td><a href="${data.link}">${data.track} - ${data.artist}</a> <a id="spotify" href="${data.uri}"><i class="fab fa-spotify" data-toggle="tooltip" data-placement="top" title="Open in Spotify"></i></a></td>
       <td>${data.reqBy}</td>
       <td> <button class="delete btn btn-danger btn-sm" data-srID="${data.id}"> <i class="fas fa-minus-circle"></i> </button> </td>
     `
-    srContainer.append(srElement)
+    srContainer.prepend(srElement)
   } catch (err) {
     console.error(err)
   }
@@ -41,3 +35,7 @@ $(function(){
       $(this).append(arr);
   });
 });
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
