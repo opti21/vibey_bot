02ea5037a9bcba49f9a2b4c98128f535b660a931
Brandon Rowe <browe.media@gmail.com>
2019-10-31 22:30:53 -0500

    switch on production pushers

--- public/js/poll-widget.js
+++ public/js/poll-widget.js
@@ -1,15 +1,15 @@
 
 // Pusher Poduction *** UNCOMMENT THIS BEFORE COMMIT ***
-// var pusher = new Pusher('94254303a6a5048bf191', {
-//   cluster: 'us2',
-//   forceTLS: true
-// });
-
-// // Pusher DEVELOPMENT
-var pusher = new Pusher('41f9377a48633b3302ff', {
+var pusher = new Pusher('94254303a6a5048bf191', {
   cluster: 'us2',
   forceTLS: true
 });
+
+// // Pusher DEVELOPMENT
+// var pusher = new Pusher('41f9377a48633b3302ff', {
+//   cluster: 'us2',
+//   forceTLS: true
+// });
 
 //Subscribe to Poll Channel
 var channel = pusher.subscribe('pollCh');
